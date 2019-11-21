0c33b285c146c6dcf1ceafb9ae53b0095bb106d0
BIT Student <classroom@bgit.rs>
2019-11-21 13:11:05 +0100

    nesto

--- aplikacija,JS,ajax/JS/controlerModule.js
+++ aplikacija,JS,ajax/JS/controlerModule.js
@@ -2,17 +2,18 @@ import { fetchUsers } from './dataModule'
 import { getSearchText, displayList } from './uiModule'
 
 
-const controllerModule = (function (ui, data) {
-    const $button = document.querySelector('.button');
-    const $list = document.querySelector('.search-result')
+const $button = document.querySelector('.button');
+const $list = document.querySelector('.search-result')
 
-    $button.addEventListener('click', function () {
-        const text = ui.getSearchText()
+$button.addEventListener('click', function () {
+    const text = ui.getSearchText()
 
-        data.fetchUsers(text, function (users) {
-            ui.displayList(users)
+    data.fetchUsers(text, function (users) {
+        ui.displayList(users)
 
-        })
     })
+})
 
-})(uiModule, dataModule)
+export {
+    init
+}
