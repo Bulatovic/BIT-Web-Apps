a923554aee9713f7501ee92b16dba89402d10d50
BIT Student <classroom@bgit.rs>
2019-11-27 17:56:14 +0100

    malovise'

---
+++ react-application/src/View/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import './view/App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-application/src/View/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer(props) {
+    return (
+
+        <h1>
+            Dobar dan tugo..
+        </h1>
+
+    );
+}
+export default Footer
