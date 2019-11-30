70b010c817a9c0a1908d1064e36729786fa723de
BIT Student <classroom@bgit.rs>
2019-11-30 17:13:26 +0100

    uradjen search

--- bitpeople2/src/view/Users/UsersPage.js
+++ bitpeople2/src/view/Users/UsersPage.js
@@ -10,9 +10,17 @@ export default class UsersPage extends Component {
 
         this.state = {
             isGrid: false,
+            query: "",
             users: []
         }
+        //this.onChange = this.onChange
     }
+    handleInputChange = (event) => {
+        this.setState({
+            query: event.target.value
+        })
+    }
+
 
     componentDidMount() {
         this.loadUsers()
@@ -33,18 +41,21 @@ export default class UsersPage extends Component {
 
 
     render() {
+        const filteredUsers = this.state.users.filter(a => a.name.toLowerCase().includes(this.state.query));
+
         return (
             <div>
                 <h1>Users</h1>
                 <a href="#" onClick={this.changeLayout}><i class="material-icons">view_module</i></a>
-                <a onClick={this.loadUsers}><i class="material-icons">refresh</i></a>
-
+                <a onClick={this.loadUsers}><i className="material-icons">refresh</i></a>
+                <input type="text" className="input" placeholder="Search" value={this.state.inputValue} onChange={this.handleInputChange} />
+                {/* <a href="sass.html"><i type="submit" value="search" className="material-icons"></i></a> */}
                 {/* <ActionButtons onReload={this.loadUsers} onLayoutChange={this.changeLayout} /> */}
 
                 {
                     this.state.isGrid
-                        ? <GridList users={this.state.users} sdlfkjsf={this.changeLayout} />
-                        : <UserList users={this.state.users} />
+                        ? <GridList users={filteredUsers} />
+                        : <UserList users={filteredUsers} />
                 }
             </div >
         )
