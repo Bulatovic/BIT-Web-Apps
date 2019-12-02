7bf92b6bbc13c6a9e7cf67e0470c30f2e137fb17
BIT Student <classroom@bgit.rs>
2019-12-02 16:02:36 +0100

    sreca

--- bitpeople2/src/view/Users/UserListItem.js
+++ bitpeople2/src/view/Users/UserListItem.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 const UserListItem = (props) => {
+
     return (
-        <li className="collection-item avatar">
+        <li className="collection-item avatar ">
             <img src={props.user.picture} alt="" className="circle responsive-img" />
             <span className="title"> <i class="tiny material-icons">person</i>  {props.user.getFullName()}</span>
             <p> <i class="tiny material-icons">cake</i>  {props.user.getBirthday()}</p>
